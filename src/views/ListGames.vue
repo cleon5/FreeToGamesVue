ca24e2254f0ed9d9200bbf33ef4f329661949311
cleon5 <cleon5@ucol.mx>
2023-04-01 13:42:29 -0600

    Add Select filter

--- src/views/ListGames.vue
+++ src/views/ListGames.vue
@@ -8,6 +8,7 @@ export default {
     data(){
         return{
             Games:[],
+            FilterGames : []
         }
     },
     components:{
@@ -22,9 +23,21 @@ export default {
                 headers: Headers
             }).then(data => {
                  this.Games = (data.data);
+                 this.FilterGames = (data.data)
             })
-            console.log(this.Games)
-        } 
+        } ,
+        onChange(ev){
+            console.log(ev.target.value)
+            this.FilterGames= this.Games.filter(game => 
+                game.platform.toLowerCase().includes(ev.target.value.toLowerCase())
+        )
+        },
+        onChangeGen(ev){
+            console.log(ev.target.value)
+            this.FilterGames= this.Games.filter(game => 
+                game.genre.toLowerCase().includes(ev.target.value.toLowerCase())
+        )
+        }
     }
 }
     
@@ -33,8 +46,40 @@ export default {
 
 </script>
 <template>
+
     <h1 class="text-center text-6xl underline text-white m-8"> Lista de juegos</h1>
+<div class="filter flex justify-around" >
+        <div class="select w-[35%] text-center">
+            <label for="Plataforma" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Plataforma</label>
+            <select @change="onChange($event)" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                <option value="">All</option>
+                <option value="pc">Pc</option>
+                <option value="browser">Browser</option>
+                
+            </select>
+        </div>
+        <div class="select w-[35%] text-center">
+            <label for="Genero" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Genero</label>
+            <select @change="onChangeGen($event)" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                <option value="" selected>Categoria</option>
+                <option value="ARPG">ARPG</option>
+                <option value="strategy">Strategy</option>
+                <option value="shooter">Shooter</option>
+                <option value="Fighting">Fighting</option>
+                <option value="racing">Racing</option>
+                <option value="social">Social</option>
+                <option value="sports">Sports</option>
+                <option value="moba">Moba</option>
+                <option value="Action">Action</option>
+                <option value="mmorpg">Mmorpg</option>
+                <option value="fantasy">Fantasy</option>
+                <option value="Battle Royale">Battle Royale</option>
+            </select>
+        </div>
+        
+    
+    </div>
     <div class="m-3 grid gap-3 grid-cols-2 sm:grid-cols-4">
-        <GameComp v-for="game in Games" :key="game.id" :game="game"/>
+        <GameComp v-for="game in FilterGames" :key="game.id" :game="game"/>
     </div>
 </template>
